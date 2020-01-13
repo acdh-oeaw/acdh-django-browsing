a3fb95a0b1f250ae59025d90a9971a2737740e77
csae8092 <peter.andorfer@oeaw.ac.at>
2020-01-13 14:50:38 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,26 @@
+=============================
+acdh-django-browsing
+=============================
+
+.. image:: https://badge.fury.io/py/acdh-django-browsing.svg
+    :target: https://badge.fury.io/py/acdh-django-browsing
+
+Django-App providing some useful things to create browsing views
+
+
+Quickstart
+----------
+
+Install acdh-django-browsing:
+
+    pip install acdh-django-browsing
+
+Add it to your `INSTALLED_APPS`:
+
+.. code-block:: python
+
+    INSTALLED_APPS = (
+        ...
+        'browsing',
+        ...
+    )
---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name='acdh-django-browsing',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Django-App providing some useful things to create browsing views',
+    long_description=README,
+    url='https://github.com/acdh-oeaw/acdh-django-browsing',
+    author='Peter Andorfer',
+    author_email='peter.andorfer@oeaw.ac.at',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'Django>=3.0',
+        'django-autocomplete-light>=3.3.5,'
+        'django-crispy-forms>=1.7.2',
+        'django-filter>=2.1.0',
+        'django-super-deduper>=0.1.2',
+        'django-tables2>=2.0.6',
+    ],
+)
