c0fb3a47d9bf720f8448891bd48db5667b41cc2a
csae8092 <peter.andorfer@oeaw.ac.at>
2022-11-29 14:24:02 +0100

    fix: column selectors shows verbose names #4

--- browsing/browsing_utils.py
+++ browsing/browsing_utils.py
@@ -10,7 +10,7 @@ from crispy_forms.layout import Submit
 from django_tables2.export.views import ExportMixin
 
 
-if 'charts' in settings.INSTALLED_APPS:
+if "charts" in settings.INSTALLED_APPS:
     from charts.models import ChartConfig
     from charts.views import create_payload
 
@@ -22,47 +22,45 @@ input_form = """
 
 
 class MergeColumn(django_tables2.Column):
-    """ renders a column with to checkbox - used to select objects for merging """
+    """renders a column with to checkbox - used to select objects for merging"""
 
     def __init__(self, *args, **kwargs):
         super(MergeColumn, self).__init__(*args, **kwargs)
 
     def render(self, value):
-        return mark_safe(
-            input_form.format(value, value)
-        )
+        return mark_safe(input_form.format(value, value))
 
 
 def get_entities_table(model_class):
-
     class GenericEntitiesTable(django_tables2.Table):
         id = django_tables2.LinkColumn()
 
         class Meta:
             model = model_class
             attrs = {"class": "table table-hover table-striped table-condensed"}
+
     return GenericEntitiesTable
 
 
 class GenericFilterFormHelper(FormHelper):
-
     def __init__(self, *args, **kwargs):
         super(GenericFilterFormHelper, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.form_class = 'genericFilterForm'
-        self.form_method = 'GET'
+        self.form_class = "genericFilterForm"
+        self.form_method = "GET"
         self.helper.form_tag = False
-        self.add_input(Submit('Filter', 'Search'))
+        self.add_input(Submit("Filter", "Search"))
 
 
 class GenericListView(ExportMixin, django_tables2.SingleTableView):
     filter_class = None
     formhelper_class = None
-    context_filter_name = 'filter'
+    context_filter_name = "filter"
     paginate_by = 50
-    template_name = 'browsing/generic_list.html'
+    template_name = "browsing/generic_list.html"
     init_columns = []
     enable_merge = False
+    excluded_cols = []
 
     def get_table_class(self):
         if self.table_class:
@@ -71,7 +69,9 @@ class GenericListView(ExportMixin, django_tables2.SingleTableView):
             return get_entities_table(self.model)
 
     def get_all_cols(self):
-        all_cols = list(self.get_table().base_columns.keys())
+        all_cols = {
+            key: value.header for key, value in self.get_table().base_columns.items()
+        }
         return all_cols
 
     def get_queryset(self, **kwargs):
@@ -91,44 +91,48 @@ class GenericListView(ExportMixin, django_tables2.SingleTableView):
 
     def get_context_data(self, **kwargs):
         context = super(GenericListView, self).get_context_data()
-        context['enable_merge'] = self.enable_merge
-        togglable_colums = [x for x in self.get_all_cols() if x not in self.init_columns]
-        context['togglable_colums'] = togglable_colums
+        context["enable_merge"] = self.enable_merge
+        togglable_colums = {
+            key: value
+            for key, value in self.get_all_cols().items()
+            if key not in self.init_columns and key not in self.exclude_columns
+        }
+        print(togglable_colums)
+        context["togglable_colums"] = togglable_colums
         context[self.context_filter_name] = self.filter
-        context['docstring'] = "{}".format(self.model.__doc__)
+        context["docstring"] = "{}".format(self.model.__doc__)
         if self.model._meta.verbose_name_plural:
-            context['class_name'] = "{}".format(self.model._meta.verbose_name.title())
+            context["class_name"] = "{}".format(self.model._meta.verbose_name.title())
         else:
-            if self.model.__name__.endswith('s'):
-                context['class_name'] = "{}".format(self.model.__name__)
+            if self.model.__name__.endswith("s"):
+                context["class_name"] = "{}".format(self.model.__name__)
             else:
-                context['class_name'] = "{}s".format(self.model.__name__)
+                context["class_name"] = "{}s".format(self.model.__name__)
         try:
-            context['create_view_link'] = self.model.get_createview_url()
+            context["create_view_link"] = self.model.get_createview_url()
         except AttributeError:
-            context['create_view_link'] = None
+            context["create_view_link"] = None
         model_name = self.model.__name__.lower()
-        context['entity'] = model_name
-        context['app_name'] = self.model._meta.app_label
-        if 'charts' in settings.INSTALLED_APPS:
+        context["entity"] = model_name
+        context["app_name"] = self.model._meta.app_label
+        if "charts" in settings.INSTALLED_APPS:
             model = self.model
             app_label = model._meta.app_label
             print(app_label)
             filtered_objs = ChartConfig.objects.filter(
-                model_name=model.__name__.lower(),
-                app_name=app_label
+                model_name=model.__name__.lower(), app_name=app_label
             )
-            context['vis_list'] = filtered_objs
-            context['property_name'] = self.request.GET.get('property')
-            context['charttype'] = self.request.GET.get('charttype')
-            if context['charttype'] and context['property_name']:
+            context["vis_list"] = filtered_objs
+            context["property_name"] = self.request.GET.get("property")
+            context["charttype"] = self.request.GET.get("charttype")
+            if context["charttype"] and context["property_name"]:
                 qs = self.get_queryset()
                 chartdata = create_payload(
-                    context['entity'],
-                    context['property_name'],
-                    context['charttype'],
+                    context["entity"],
+                    context["property_name"],
+                    context["charttype"],
                     qs,
-                    app_label=app_label
+                    app_label=app_label,
                 )
                 context = dict(context, **chartdata)
         return context
@@ -136,37 +140,37 @@ class GenericListView(ExportMixin, django_tables2.SingleTableView):
 
 class BaseDetailView(DetailView):
     model = None
-    template_name = 'browsing/generic_create.html'
+    template_name = "browsing/generic_create.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['docstring'] = "{}".format(self.model.__doc__)
-        context['class_name'] = "{}".format(self.model.__name__)
-        context['app_name'] = "{}".format(self.model._meta.app_label)
+        context["docstring"] = "{}".format(self.model.__doc__)
+        context["class_name"] = "{}".format(self.model.__name__)
+        context["app_name"] = "{}".format(self.model._meta.app_label)
         return context
 
 
 class BaseCreateView(CreateView):
     model = None
     form_class = None
-    template_name = 'browsing/generic_create.html'
+    template_name = "browsing/generic_create.html"
 
     def get_context_data(self, **kwargs):
         context = super(BaseCreateView, self).get_context_data()
-        context['docstring'] = "{}".format(self.model.__doc__)
-        context['class_name'] = "{}".format(self.model.__name__)
+        context["docstring"] = "{}".format(self.model.__doc__)
+        context["class_name"] = "{}".format(self.model.__name__)
         return context
 
 
 class BaseUpdateView(UpdateView):
     model = None
     form_class = None
-    template_name = 'browsing/generic_create.html'
+    template_name = "browsing/generic_create.html"
 
     def get_context_data(self, **kwargs):
         context = super(BaseUpdateView, self).get_context_data()
-        context['docstring'] = "{}".format(self.model.__doc__)
-        context['class_name'] = "{}".format(self.model.__name__)
+        context["docstring"] = "{}".format(self.model.__doc__)
+        context["class_name"] = "{}".format(self.model.__name__)
         # if self.model.__name__.endswith('s'):
         #     context['class_name'] = "{}".format(self.model.__name__)
         # else:
@@ -183,49 +187,49 @@ def model_to_dict(instance):
     for x in instance._meta.get_fields():
         f_type = "{}".format(type(x))
         field_dict = {
-                "name": x.name,
-                "help_text": getattr(x, 'help_text', ''),
-            }
+            "name": x.name,
+            "help_text": getattr(x, "help_text", ""),
+        }
         try:
-            field_dict['extra_fields'] = x.extra
+            field_dict["extra_fields"] = x.extra
         except AttributeError:
-            field_dict['extra_fields'] = None
-        if 'reverse_related' in f_type:
+            field_dict["extra_fields"] = None
+        if "reverse_related" in f_type:
             values = getattr(instance, x.name, None)
             if values is not None:
-                field_dict['value'] = values.all()
+                field_dict["value"] = values.all()
             else:
-                field_dict['value'] = []
-            if getattr(x, 'related_name', None) is not None:
-                field_dict['verbose_name'] = getattr(x, 'related_name', x.name)
+                field_dict["value"] = []
+            if getattr(x, "related_name", None) is not None:
+                field_dict["verbose_name"] = getattr(x, "related_name", x.name)
             else:
-                field_dict['verbose_name'] = getattr(x, 'verbose_name', x.name)
-            field_dict['f_type'] = 'REVRESE_RELATION'
-        elif 'related.ForeignKey' in f_type:
-            field_dict['verbose_name'] = getattr(x, 'verbose_name', x.name)
-            field_dict['value'] = getattr(instance, x.name, '')
-            field_dict['f_type'] = 'FK'
-        elif 'TreeForeignKey' in f_type:
-            field_dict['verbose_name'] = getattr(x, 'verbose_name', x.name)
-            field_dict['value'] = getattr(instance, x.name, '')
-            field_dict['f_type'] = 'FK'
-        elif 'related.ManyToManyField' in f_type:
+                field_dict["verbose_name"] = getattr(x, "verbose_name", x.name)
+            field_dict["f_type"] = "REVRESE_RELATION"
+        elif "related.ForeignKey" in f_type:
+            field_dict["verbose_name"] = getattr(x, "verbose_name", x.name)
+            field_dict["value"] = getattr(instance, x.name, "")
+            field_dict["f_type"] = "FK"
+        elif "TreeForeignKey" in f_type:
+            field_dict["verbose_name"] = getattr(x, "verbose_name", x.name)
+            field_dict["value"] = getattr(instance, x.name, "")
+            field_dict["f_type"] = "FK"
+        elif "related.ManyToManyField" in f_type:
             values = getattr(instance, x.name, None)
             if values is not None:
-                field_dict['value'] = values.all()
+                field_dict["value"] = values.all()
             else:
-                field_dict['value'] = []
-            field_dict['verbose_name'] = getattr(x, 'verbose_name', x.name)
-            field_dict['f_type'] = 'M2M'
-        elif 'fields.DateTimeField' in f_type:
-            field_value = getattr(instance, x.name, '')
-            field_dict['verbose_name'] = getattr(x, 'verbose_name', x.name)
-            field_dict['f_type'] = 'DateTime'
+                field_dict["value"] = []
+            field_dict["verbose_name"] = getattr(x, "verbose_name", x.name)
+            field_dict["f_type"] = "M2M"
+        elif "fields.DateTimeField" in f_type:
+            field_value = getattr(instance, x.name, "")
+            field_dict["verbose_name"] = getattr(x, "verbose_name", x.name)
+            field_dict["f_type"] = "DateTime"
             if field_value:
-                field_dict['value'] = (field_value.strftime("%Y-%m-%d %H:%M:%S"))
+                field_dict["value"] = field_value.strftime("%Y-%m-%d %H:%M:%S")
         else:
-            field_dict['verbose_name'] = getattr(x, 'verbose_name', x.name)
-            field_dict['value'] = f"{getattr(instance, x.name, '')}"
-            field_dict['f_type'] = 'SIMPLE'
+            field_dict["verbose_name"] = getattr(x, "verbose_name", x.name)
+            field_dict["value"] = f"{getattr(instance, x.name, '')}"
+            field_dict["f_type"] = "SIMPLE"
         field_dicts.append(field_dict)
     return field_dicts
