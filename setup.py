f53f754752c6803f9e5577d12e4f1c8725fd70ee
csae8092 <p.andorfer@gmail.com>
2024-09-29 09:09:24 +0200

    load base template from root dir

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="acdh-django-browsing",
-    version="2.0",
+    version="3.0",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
