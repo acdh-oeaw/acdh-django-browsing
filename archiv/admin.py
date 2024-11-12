b84923c589a982ba74c80bbbfd056b7870351ab6
csae8092 <p.andorfer@gmail.com>
2024-11-12 08:11:44 +0100

    adds datamodel for example project

--- archiv/admin.py
+++ archiv/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from archiv.models import Person, Place, Book
+
+admin.site.register(Person)
+admin.site.register(Place)
+admin.site.register(Book)
