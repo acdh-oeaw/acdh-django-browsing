7b2d164e9bbf25a3bcc42e6eb72a9131f4efeabb
csae8092 <p.andorfer@gmail.com>
2024-11-13 10:18:11 +0100

    set up tests (actually refers to tests #20)

--- djangobaseproject/settings.py
+++ djangobaseproject/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "django_tables2",
     "crispy_forms",
     "crispy_bootstrap5",
